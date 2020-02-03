5585c7be4167823716c39fb15248d30f4d11b5a4
Nikita Silchenko <nikitiuss@gmail.com>
2020-02-03 15:43:08 +0200

    commit

---
+++ Problems (6)/Printing strings in the same order/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine();
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(n1);
+        System.out.println(n2);
+    }
+}
